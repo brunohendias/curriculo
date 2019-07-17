24edfa44e30959ba1d699721b4cefadac5761ff6
brunohendias <brunohen.dias@gmail.com>
2019-07-17 19:39:02 -0300

    info

--- src/components/left/contato.vue
+++ src/components/left/contato.vue
@@ -1,11 +1,14 @@
 <template>
 	<div>
 		<h1>Contato</h1>
-		<a href="https://github.com/brunohendias?tab=repositories" target="_blank">
-			<img src="https://img.icons8.com/ios/50/000000/github-filled.png">
+		<a href="https://github.com/brunohendias?tab=repositories" alt="Github" target="_blank">
+			<img src="https://img.icons8.com/ios/50/000000/github-filled.png"><span>Github</span>
 		</a>
-		<a href="https://www.facebook.com/profile.php?id=100013873149290" target="_blank">
-			<img src="https://img.icons8.com/ios/50/000000/facebook-filled.png">
+		<a href="https://www.facebook.com/profile.php?id=100013873149290" alt="Facebook" target="_blank">
+			<img src="https://img.icons8.com/cute-clipart/50/000000/facebook.png"><span>Facebook</span>
+		</a>
+		<a href="https://bit.ly/32zU6Lk">
+			<img src="https://img.icons8.com/color/48/000000/whatsapp.png" alt="Whatssap"><span>Whatssap</span>
 		</a>
 	</div>
 </template>
