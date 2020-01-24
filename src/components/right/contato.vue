a9f9197d221e967fc781141632fe6ed9bc00bfbd
bruno.h.dias <bruno.h.dias@animaeducacao.com.br>
2020-01-23 22:36:23 -0300

    icons

--- src/components/right/contato.vue
+++ src/components/right/contato.vue
@@ -1,18 +1,40 @@
 <template>
-	<div id="contato" align="center" class="mb-5">
+	<div id="contato" class="mb-5">
 		<h1>Contato</h1>
-		<a href="https://github.com/brunohendias?tab=repositories" class="pr-3">
-			<img src="https://img.icons8.com/ios/48/000000/github-filled.png" alt="Github"><span>Github</span>
-		</a>
-		<a href="https://www.facebook.com/profile.php?id=100013873149290" class="pl-2">
-			<img src="https://img.icons8.com/color/48/000000/facebook.png" alt="Facebook"><span>Facebook</span>
-		</a><br>
-		<a href="https://www.linkedin.com/in/bruno-dias-a5a60917a" class="p-1">
-			<img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="linkedin">Linkedin
-		</a>
-		<a href="https://bit.ly/32z5E1J" class="p-1">
-			<img src="https://img.icons8.com/color/48/000000/whatsapp.png" alt="Whatssap"><span>Whatssap</span>
-		</a>
+		<div class="col-sm-12">
+			<div class="row">
+				<div class="col-sm-6 m-auto">
+					<a href="https://github.com/brunohendias?tab=repositories">
+						<font color="#000" size="45">
+							<i class="fab fa-github-square" alt="Github"></i>
+						</font>
+						<span class="p-1">Github</span>
+					</a>
+					<a href="https://www.facebook.com/profile.php?id=100013873149290" class="float-right">
+						<span class="p-1">Facebook</span>
+						<font color="#00f" size="45">
+							<i class="fab fa-facebook-square" alt="Facebook"></i>
+						</font>
+					</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-6 m-auto">
+					<a href="https://www.linkedin.com/in/bruno-dias-a5a60917a">
+						<font size="45">
+							<i class="fab fa-linkedin" alt="Linkedin"></i>
+						</font>
+						<span class="p-1">Linkedin</span>
+					</a>
+					<a href="https://bit.ly/32z5E1J" class="float-right">
+						<span class="p-1">Whatssap</span>
+						<font color="#0d0" size="45">
+							<i class="fab fa-whatsapp-square" alt="Whatssap"></i>
+						</font>
+					</a>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
--- src/components/right/projetos.vue
+++ src/components/right/projetos.vue
@@ -5,6 +5,15 @@
 			<b-list-group-item href="https://brunohendias.github.io/Sambox/">
 				<span>Sambox - Site para empresa Sam Box, feito com: Vue, Bootstrap-vue, jquery usando pluguin(jquery-easing)</span>
 			</b-list-group-item>
+			<b-list-group-item href="https://brunohendias.github.io/stocktrader/">
+				<span>Trader - Compra e venda de ações feito com: Vue, Vuetify, axios</span>
+			</b-list-group-item>
+			<b-list-group-item href="https://brunohendias.github.io/formulario/">
+				<span>Formulario de cadastro feito com: Vue, axios e bootstrap 4</span>
+			</b-list-group-item>
+			<b-list-group-item href="https://brunohendias.github.io/tarefastodo/">
+				<span>Tarefas TODO, organizar o dia mais facil guardando as tarefas, feito com: Vue e bootstrap 4</span>
+			</b-list-group-item>
 			<b-list-group-item href="https://brunohendias.github.io/Vingadores/">
 				<span>Vingadores - Land Page do filme Vingadores, feito com: Bootstrap-4 e Jquery usando pluguin(jquery-easing)</span>
 			</b-list-group-item>
@@ -13,12 +22,6 @@
 			</b-list-group-item>
 			<b-list-group-item href="https://brunohendias.github.io/cadastroAluno/">
 				<span>Cadastro Aluno - formulario para cadastro de aluno e notas feito com: Vue, Bootstrap 4</span>
-			</b-list-group-item>
-			<b-list-group-item href="https://brunohendias.github.io/stocktrader/">
-				<span>Trader - Compra e venda de ações feito com: Vue, Vuetify, axios</span>
-			</b-list-group-item>
-			<b-list-group-item href="https://brunohendias.github.io/formulario/">
-				<span>Formulario de cadastro feito com: Vue, axios e bootstrap 4</span>
 			</b-list-group-item>
 			<b-list-group-item href="https://github.com/brunohendias/Python/">
 				<span>Python - Exercicios e mini jogos para praticar</span>
